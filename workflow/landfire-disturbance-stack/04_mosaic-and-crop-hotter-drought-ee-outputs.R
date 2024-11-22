3cb5fcc07153ae7291cde0b028118e3e345164d8
TylerLMcIntosh <tylermcintosh7@gmail.com>
2024-11-22 00:20:44 +0000

    add remote download of gee data

--- workflow/landfire-disturbance-stack/04_mosaic-and-crop-hotter-drought-ee-outputs.R
+++ workflow/landfire-disturbance-stack/04_mosaic-and-crop-hotter-drought-ee-outputs.R
@@ -1,16 +1,96 @@
 # Mosaic hotter drought data
-
+install.packages("here")
 library(terra)
 library(dplyr)
+library(googledrive)
+library(purrr)
+library(here)
 
-ee_data_dir <- "D:/google-drive_uc-davis/My Drive/ee/"
+driveFolder <- 'GEE_Exports'
+googledrive::drive_auth()  
+
+cyverse <- TRUE
+if(cyverse) {
+  system("cp -r ~/data-store/data/iplant/home/shared/earthlab/macrosystems/disturbance-stack/data ~/forest-disturbance-stack/")
+}
+
+
+#' Download a file from Google Drive to a local directory
+#'
+#' This function downloads a file from a Google Drive path to a specified local path.
+#'
+#' @param gDrivePath A character string. The path or name of the file on Google Drive.
+#' @param localPath A character string. The local path where the file will be saved.
+#' @param overwrite A logical value indicating whether to overwrite the file if it already exists at the local path. Defaults to `TRUE`.
+#'
+#' @details This function retrieves a file's ID from Google Drive using the provided `gDrivePath` and downloads it to the local directory specified by `localPath`. The file will be overwritten if `overwrite` is set to `TRUE` (default).
+#' 
+#' @return The downloaded file will be saved to the specified `localPath`.
+#' 
+#' @note You must be authenticated with Google Drive via the `googledrive` package for this function to work.
+#' 
+#' @importFrom googledrive drive_get drive_download as_id
+#' 
+#' @examples
+#' \dontrun{
+#' # Example usage:
+#' download_data_from_gdrive("path/to/file/on/drive", "path/to/local/file.csv")
+#' }
+#' 
+#' @export
+download_data_from_gdrive <- function(gDrivePath, localPath) {
+  # Validate inputs
+  if (missing(gDrivePath) || missing(localPath)) {
+    stop("Both 'gDrivePath' and 'localPath' must be provided.")
+  }
+  if (!is.character(gDrivePath) || !nzchar(gDrivePath)) {
+    stop("'gDrivePath' must be a non-empty string.")
+  }
+  if (!is.character(localPath) || !nzchar(localPath)) {
+    stop("'localPath' must be a non-empty string.")
+  }
+  
+  # Retrieve file ID from GDrive
+  f <- googledrive::drive_get(gDrivePath)
+  id <- f$id
+  nm <- f$name
+  
+  googledrive::drive_download(googledrive::as_id(id), path = localPath, overwrite = TRUE)
+}
+
+# A function to read a good fire data file
+download.hotdrought.csv.from.gdrive <- function(file_name) {
+  path <- paste0("~/", driveFolder, "/", file_name)
+  csv <- download_data_from_gdrive(gDrivePath = path, localPath = here::here('data', 'tmp', file_name))
+  return(csv)
+}
+
+folder <- drive_get(driveFolder)
+
+# Check if the folder was found
+if (nrow(folder) == 0) {
+  stop("Folder not found. Check the name or ID.")
+}
+
+# List files in the folder that start with "hammond"
+files_in_folder <- drive_ls(
+  path = as_id(folder$id), # Use the folder's ID
+  pattern = "^hammond"     # Regex for files starting with "hammond"
+)
+
+
+purrr::map(.x = files_in_folder$name,
+           .f = download.hotdrought.csv.from.gdrive)
+
+
+#ee_data_dir <- "D:/google-drive_uc-davis/My Drive/ee/"
 
 hotter_drought_dir <- "data/out/hotter-drought/western-conus"
 dir.create(hotter_drought_dir, recursive = TRUE, showWarnings = FALSE)
 
 years <- 1999:2020
 
-drought_files <- list.files(ee_data_dir, pattern = "hammond-hotter-drought", full.names = TRUE)
+drought_files <- list.files(here::here('data', 'tmp'), pattern = "hammond-hotter-drought", full.names = TRUE)
 
 for (i in seq_along(years)) {
   
